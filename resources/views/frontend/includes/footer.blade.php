358576a083e5d2fd6b7a4dcfc2123f81fd8ecdf7
unbolt <dan@unbo.lt>
2016-07-12 16:20:53 +0100

    Adding favicons for all resolutions. Fixes #12

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,11 +1,22 @@
 <section class="footer hidden-sm hidden-xs">
     <div class="container">
-        <div class="col-md-4 col-md-offset-2">
+        <div class="col-md-2 col-md-offset-2 text-center">
             <img src="/images/weiwu_notext.png" />
         </div>
-        <div class="col-md-4">
-            <i class="fa fa-twitter fa-4x" aria-hidden="true"></i>
-            <i class="fa fa-youtube-play fa-4x" aria-hidden="true"></i>
+        <div class="col-md-2">
+            <ol>
+                <li>{!! link_to_route('frontend.index', 'Home') !!}</li>
+                <li>{!! link_to_route('frontend.roster', 'Roster') !!}</li>
+                <li>{!! link_to('forums', 'Forums') !!}</li>
+                @roles('Raider')
+                    <li>{!! link_to('raids', 'Raids') !!}</li>
+                @endauth
+            </ol>
+        </div>
+        <div class="col-md-4 text-center">
+            <a href="https://twitter.com/weiwuweiguild" target="_blank"><i class="fa fa-twitter fa-4x" aria-hidden="true"></i></a>
+            &nbsp; &nbsp;
+            <a href="https://www.youtube.com/playlist?list=PLmx_2Q_Isfg2pwUo5_BZmOoy8P1l1XZi8" target="_blank"><i class="fa fa-youtube-play fa-4x" aria-hidden="true"></i></a>
 
         </div>
     </div>
